47cda7629b6b00d2b971609581f1f63b9c83c8a6
Karolis Vilpisauskas <karolis.vilpisauskas@gmail.com>
2019-12-11 01:47:48 +0200

    - Final

--- js/main.js
+++ js/main.js
@@ -1,84 +1,77 @@
+/* eslint-disable */
 // UI elements.
-const deviceNameLabel = document.getElementById('device-name');
-const connectButton = document.getElementById('connect');
-const disconnectButton = document.getElementById('disconnect');
-const terminalContainer = document.getElementById('terminal');
-const sendForm = document.getElementById('send-form');
-const inputField = document.getElementById('input');
+const connectButton = document.getElementById("connect");
+const disconnectButton = document.getElementById("disconnect");
+const sendForm = document.getElementById("send-form");
+const inputField = document.getElementById("input");
 
-// Helpers.
-const defaultDeviceName = 'Terminal';
-const terminalAutoScrollingLimit = terminalContainer.offsetHeight / 2;
-let isTerminalAutoScrolling = true;
-
-const scrollElement = (element) => {
-  const scrollTop = element.scrollHeight - element.offsetHeight;
-
-  if (scrollTop > 0) {
-    element.scrollTop = scrollTop;
-  }
-};
-
-const logToTerminal = (message, type = '') => {
-  terminalContainer.insertAdjacentHTML('beforeend',
-      `<div${type && ` class="${type}"`}>${message}</div>`);
-
-  if (isTerminalAutoScrolling) {
-    scrollElement(terminalContainer);
-  }
-};
+// Movement buttons
+const forwardBtn = document.getElementById("forward");
+const backBtn = document.getElementById("back");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
 
 // Obtain configured instance.
 const terminal = new BluetoothTerminal();
 
-// Override `receive` method to log incoming data to the terminal.
-terminal.receive = function(data) {
-  logToTerminal(data, 'in');
-};
-
-// Override default log method to output messages to the terminal and console.
-terminal._log = function(...messages) {
-  // We can't use `super._log()` here.
-  messages.forEach((message) => {
-    logToTerminal(message);
-    console.log(message); // eslint-disable-line no-console
-  });
-};
-
 // Implement own send function to log outcoming data to the terminal.
-const send = (data) => {
-  terminal.send(data).
-      then(() => logToTerminal(data, 'out')).
-      catch((error) => logToTerminal(error));
+const send = data => {
+  terminal.send(data);
 };
 
 // Bind event listeners to the UI elements.
-connectButton.addEventListener('click', () => {
-  terminal.connect().
-      then(() => {
-        deviceNameLabel.textContent = terminal.getDeviceName() ?
-            terminal.getDeviceName() : defaultDeviceName;
-      });
+connectButton.addEventListener("click", () => {
+  terminal.connect();
 });
 
-disconnectButton.addEventListener('click', () => {
+disconnectButton.addEventListener("click", () => {
   terminal.disconnect();
-  deviceNameLabel.textContent = defaultDeviceName;
 });
 
-sendForm.addEventListener('submit', (event) => {
-  event.preventDefault();
+// Walking events
 
-  send(inputField.value);
+const holdEvent = (btn, action) => {
+  var t;
 
-  inputField.value = '';
-  inputField.focus();
-});
+  const repeat = () => {
+    action();
+    t = setTimeout(repeat, 1000);
+  };
 
-// Switch terminal auto scrolling if it scrolls out of bottom.
-terminalContainer.addEventListener('scroll', () => {
-  const scrollTopOffset = terminalContainer.scrollHeight -
-      terminalContainer.offsetHeight - terminalAutoScrollingLimit;
+  btn.addEventListener("touchstart", () => {
+    repeat();
+  });
 
-  isTerminalAutoScrolling = (scrollTopOffset < terminalContainer.scrollTop);
-});
+  btn.addEventListener("mousedown", () => {
+    repeat();
+  });
+
+  btn.addEventListener("touchend", () => {
+    clearTimeout(t);
+  });
+
+  btn.addEventListener("mouseup", () => {
+    clearTimeout(t);
+  });
+}
+
+const moveForward = () => {
+  send("7IYdr");
+}
+
+const moveBack = () => {
+  send("YjXIBe");
+}
+
+const turnLeft = () => {
+  send("teoYCVe");
+}
+
+const turnRight = () => {
+  send("AEc2Afg!5");
+}
+
+holdEvent(forwardBtn, moveForward);
+holdEvent(backBtn, moveBack);
+holdEvent(leftBtn, turnLeft);
+holdEvent(rightBtn, turnRight);
